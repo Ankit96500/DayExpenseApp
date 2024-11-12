c026f0ccea69c4d70e560ea11a15451288d59afd
ankit(ash) <ankitsharma.p413@gmail.com>
2024-11-12 18:08:22 +0530

    merge complete in release3

--- dist/routes/expenseReportR.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const expenseReportC_1 = require("../controllers/expenseReportC");
-const authorize_1 = require("../middleware/authorize");
-const router = (0, express_1.Router)();
-router.get('/user', authorize_1.UserAuthorized, expenseReportC_1.showUserExpense);
-exports.default = router;
